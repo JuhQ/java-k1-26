37b9caaf6db6e7d61a5eb7204bdc5bbfc48c2a31
Juha Tauriainen <juha@bin.fi>
2026-01-28 13:59:47 +0200

    Add esimerkki teksti method for fetchin examples

--- 4.git/src/Esimerkki.java
+++ 4.git/src/Esimerkki.java
@@ -2,4 +2,9 @@ public class Esimerkki {
     public void jee() {
         System.out.println("esimerkkiviesti");
     }
+
+    public String getEsimerkkiteksti() {
+        // Tässä kohta kuvitteellinen tietokantahaku...
+        return "esimerkkiteksti";
+    }
 }
